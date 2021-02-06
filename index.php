4f905f7f6e2afdafa22706ec5c73ef23588e2199
sako <sandeshkoirala2010@gmail.com>
2021-02-06 01:23:16 +0000

    index.php to redirect

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	include('index.html');
-	//header('Location: '.$uri.'index.html');
-	//exit;
 ?>
-Something is wrong with the HTTP server installation :-(
+
