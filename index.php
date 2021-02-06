0ecdae238b38b719c90dde55cbef9ac2902fa60e
sako <sandeshkoirala2010@gmail.com>
2021-02-06 01:08:55 +0000

    index.php to redirect

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/wwwAcrux/index.html');
+	header('Location: '.$uri.'/index.html');
 	exit;
 ?>
 Something is wrong with the HTTP server installation :-(
